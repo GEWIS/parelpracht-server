d77964163838feeecd9647813d1c4754fa4f1b21
Roy (Laptop) <r.t.b.kakkenberg@student.tue.nl>
2021-01-20 22:54:27 +0100

    Added filters to the megatable query

--- src/helpers/rawQueries.ts
+++ src/helpers/rawQueries.ts
@@ -1,9 +1,41 @@
 import { getManager } from 'typeorm';
 import { ListParams } from '../controllers/ListParams';
 
+function arrayToQueryArray(arr: string[] | number[]) {
+  let result = '(';
+  arr.forEach((a: string | number) => {
+    if (typeof a === 'string') {
+      result += `'${a}', `;
+    } else {
+      result += `${a}, `;
+    }
+  });
+  return `${result.substring(0, result.length - 2)})`;
+}
+
 export default class RawQueries {
   static getContractWithProductsAndTheirStatuses = (lp: ListParams, result: 'data' | 'count') => {
     let query = '';
+
+    let [companyFilter, productFilter, statusFilter, invoicedFilter] = ['', '', '', ''];
+    if (lp.filters) lp.filters?.forEach((f) => {
+      if (f.column === 'companyId') {
+        companyFilter = `AND company.id IN ${arrayToQueryArray(f.values)}`;
+      }
+      if (f.column === 'productId') {
+        productFilter = `AND p."productId" IN ${arrayToQueryArray(f.values)}`;
+      }
+      if (f.column === 'status') {
+        statusFilter = `AND a1."subType" IN ${arrayToQueryArray(f.values)}`;
+      }
+      if (f.column === 'invoiced') {
+        if (f.values[0]) {
+          invoicedFilter = 'AND p."invoiceId" IS NOT NULL';
+        } else {
+          invoicedFilter = 'AND p."invoiceId" IS NULL';
+        }
+      }
+    });
 
     query += `
       SELECT company.id, company.name,
@@ -16,22 +48,22 @@ export default class RawQueries {
           from (
           SELECT p.*, a1.*
           FROM product_instance p
-          JOIN product_instance_activity a1 ON (p.id = a1."productInstanceId" AND a1.type = 'STATUS')
+          JOIN product_instance_activity a1 ON (p.id = a1."productInstanceId" AND a1.type = 'STATUS' ${statusFilter})
           LEFT OUTER JOIN product_instance_activity a2 ON (p.id = a2."productInstanceId" AND
             (a1."createdAt" < a2."createdAt" OR (a1."createdAt" = a2."createdAt" AND a1.id < a2.id)) AND
              a2.type = 'STATUS')
-          WHERE (a2.id IS NULL AND contract.id = p."contractId")
+          WHERE (a2.id IS NULL AND contract.id = p."contractId" ${productFilter} ${invoicedFilter})
           ) d
         ) as products
         from contract
         where (contract."companyId" = company.id AND (
           SELECT COUNT(*)
-          FROM product_instance cp
-          JOIN product_instance_activity ca1 ON (cp.id = ca1."productInstanceId" AND ca1.type = 'STATUS')
-          LEFT OUTER JOIN product_instance_activity ca2 ON (cp.id = ca2."productInstanceId" AND
-            (ca1."createdAt" < ca2."createdAt" OR (ca1."createdAt" = ca2."createdAt" AND ca1.id < ca2.id)) AND
-             ca2.type = 'STATUS')
-          WHERE (ca2.id IS NULL AND contract.id = cp."contractId")
+          FROM product_instance p
+          JOIN product_instance_activity a1 ON (p.id = a1."productInstanceId" AND a1.type = 'STATUS' ${statusFilter})
+          LEFT OUTER JOIN product_instance_activity a2 ON (p.id = a2."productInstanceId" AND
+            (a1."createdAt" < a2."createdAt" OR (a1."createdAt" = a2."createdAt" AND a1.id < a2.id)) AND
+             a2.type = 'STATUS')
+          WHERE (a2.id IS NULL AND contract.id = p."contractId" ${productFilter} ${invoicedFilter})
           ) > 0)
       ) t
       ) as contracts
@@ -42,14 +74,15 @@ export default class RawQueries {
         WHERE (qc."companyId" = company.id)
       ) > 0 AND (
         SELECT COUNT(*)
-        FROM contract ccp
-        JOIN product_instance ccpi ON (ccpi."contractId" = ccp.id)
-        JOIN product_instance_activity cca1 ON (ccpi.id = cca1."productInstanceId" AND cca1.type = 'STATUS')
-        LEFT OUTER JOIN product_instance_activity cca2 ON (ccpi.id = cca2."productInstanceId" AND
-          (cca1."createdAt" < cca2."createdAt" OR (cca1."createdAt" = cca2."createdAt" AND cca1.id < cca2.id)) AND
-           cca2.type = 'STATUS')
-        WHERE (cca2.id IS NULL AND ccp."companyId" = company.id)
-      ) > 0)
+        FROM contract contract
+        JOIN product_instance p ON (p."contractId" = contract.id)
+        JOIN product_instance_activity a1 ON (p.id = a1."productInstanceId" AND a1.type = 'STATUS' ${statusFilter})
+        LEFT OUTER JOIN product_instance_activity a2 ON (p.id = a2."productInstanceId" AND
+          (a1."createdAt" < a2."createdAt" OR (a1."createdAt" = a2."createdAt" AND a1.id < a2.id)) AND
+           a2.type = 'STATUS')
+        WHERE (a2.id IS NULL AND contract."companyId" = company.id ${productFilter} ${invoicedFilter})
+      ) > 0) ${companyFilter}
+      order by company.name asc
     `;
 
     if (result === 'count') {
